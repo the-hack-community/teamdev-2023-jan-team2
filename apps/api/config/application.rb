0b6ef8c80d13d1e452c170e58867dc9c024a0645
zorro901 <zoro907@gmail.com>
2023-01-15 00:10:03 +0900

    :package: build(api): FactoryBotを追加

--- apps/api/config/application.rb
+++ apps/api/config/application.rb
@@ -28,7 +28,8 @@ module Api
                        controller_specs: false,
                        view_specs: false,
                        helper_specs: false,
-                       routing_specs: false
+                       routing_specs: false,
+                       fixtures: false
     end
   end
 end
