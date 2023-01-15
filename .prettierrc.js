98b45212a2033c6876e7ef67644541a6b9de5a24
zorro901 <zoro907@gmail.com>
2023-01-15 21:38:37 +0900

    :package: build(front): Prettierの設定を追加

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const config = {
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleAttributePerLine: true,
+}
+
+module.exports = config
