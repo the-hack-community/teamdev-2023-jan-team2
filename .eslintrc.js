26d240a72e4fae93c1aa7baadabce781405ea7a7
zorro901 <zoro907@gmail.com>
2023-01-15 21:41:49 +0900

    :package: build: Lint-stagedにprettierを設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
+/** @type {import("eslint").Linter.BaseConfig} */
 module.exports = {
+  ...require('@ca11-ope/config/eslint-next.js'),
   root: true,
   settings: {
     next: {
-      rootDir: ['apps/*/']
-    }
+      rootDir: ['apps/*/'],
+    },
   }
-};
+}
