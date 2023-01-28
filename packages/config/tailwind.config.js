e8f2ca7e5641476351b85030d8474c12e9d88b21
zorro901 <zoro907@gmail.com>
2023-01-28 23:04:10 +0900

    :bug: fix(front): Tailwindの設定を変更
    extendへプロパティを移動

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const { zIndex, fontSize, spacing } = require('./tailwind.custom')
+const { pxConverter } = require('./tailwind.custom')
 module.exports = {
   content: ['../../packages/ui/components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
-    padding: () => spacing(),
-    margin: () => spacing(),
-    colors: {
-      transparent: 'transparent',
-      orange: '#FF5C00',
-      yellow: '#FFD754',
-      blue: '#4E85FF',
-      navy: '#190F4A',
-    },
     extend: {
+      padding: () => pxConverter(),
+      margin: () => pxConverter(),
+      colors: {
+        transparent: 'transparent',
+        orange: '#FF5C00',
+        yellow: '#FFD754',
+        blue: '#4E85FF',
+        navy: '#190F4A',
+      },
       fontFamily: {
         zou: ['var(--font-zou)'],
       },
--- packages/config/tailwind.custom.js
+++ packages/config/tailwind.custom.js
@@ -1,4 +1,4 @@
-const spacing = () => {
+const pxConverter = () => {
   const MIN = 0
   const MAX = 100
   const INCREMENTS_NUM = 1
@@ -9,18 +9,6 @@ const spacing = () => {
   return s
 }
 
-const fontSize = () => {
-  const MIN = 10
-  const MAX = 100
-  const INCREMENTS_NUM = 1
-  const s = {}
-  for (let i = MIN; i <= MAX; i += INCREMENTS_NUM) {
-    s[`${i}px`] = `${i * 0.0625}rem`
-  }
-  return s
-}
-
 module.exports = {
-  fontSize,
-  spacing,
+  pxConverter,
 }
