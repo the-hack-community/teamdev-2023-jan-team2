a0402eee97ddbeb9790a983ec062b0170d0d13ac
zorro901 <zoro907@gmail.com>
2023-02-05 17:36:01 +0900

    :package: build(api): GraphQL関係の設定ファイルを追加
    configとspecファイルを追加

---
+++ apps/api/spec/factories/images.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :image do
+    user { nil }
+    url { 'MyString' }
+    description { 'MyString' }
+    keyword { 'MyString' }
+    prompt { 'MyString' }
+    negative_prompt { 'MyString' }
+    model { 'MyString' }
+    steps { 1 }
+    sampler { 'MyString' }
+    scale { 1.5 }
+    seed { '9.99' }
+    height { 1 }
+    width { 1 }
+    clip_skip { 1 }
+  end
+end
