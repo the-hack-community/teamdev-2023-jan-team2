cab40a628108e5219d7a084310c5f4c7b5f963dc
dtakkiy <dtakkiy00@gmail.com>
2023-02-10 13:55:35 +0900

    :sparkles: feat(front): アカウントページの作成

--- apps/web/src/app/my-profile/page.tsx
+++ apps/web/src/app/my-profile/page.tsx
@@ -1,22 +1,54 @@
+import MyProfileSection from 'ui/components/organisms/MyProfileSection'
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth/next'
 import getSingleImage from '../../libs/image-util'
 import { authOptions } from '../../pages/api/auth/[...nextauth]'
-const MyProfilePage = async () => {
+
+const MyProfilePage = async() => {
   const session = await getServerSession(authOptions)
   if (!session) redirect('/')
   const imageData = await getSingleImage(1)
+  const dummyData = {
+    user: {
+      name: 'にゃーんさん',
+      description: '自己紹介文です。',
+      icon: '/icon.png',
+      posts: [
+        {
+          image: '/sample.png',
+          url: 'http://dummy.co.jp',
+          caption: '画像キャプション1',
+        },
+        {
+          image: '/sample.png',
+          url: 'http://dummy.co.jp',
+          caption: '画像キャプション2',
+        },
+        {
+          image: '/sample.png',
+          url: 'http://dummy.co.jp',
+          caption: '画像キャプション3',
+        },
+        {
+          image: '/sample.png',
+          url: 'http://dummy.co.jp',
+          caption: '画像キャプション4',
+        },
+      ],
+    },
+  }
+
   return (
-    <>
-      <h1>MyProfilePage</h1>
+    <div>
+      <MyProfileSection user={dummyData.user} />
       <Image
         src={`data:image/png;base64,${imageData}`}
         alt=''
         width={300}
         height={300}
       />
-    </>
+    </div>
   )
 }
 
---
+++ packages/ui/components/organisms/MyProfileSection/index.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Card from 'molecules/Card'
+
+const MyProfileSection = ({ user }: MyProfileSection) => {
+  let cardBackgroundColor: 'bg-orange' | 'bg-yellow' | 'bg-blue' | 'bg-navy' = 'bg-orange'
+
+  return (
+    <section className='bg-yellow mx-auto mt-12 w-[951px] rounded-3xl pb-32'>
+      <div className='flex w-[744px] pt-8 pl-8'>
+        <Image
+          src={user.icon}
+          alt={`${user.name}-icon`}
+          width={227}
+          height={227}
+          className='h-[227px] w-[227px] rounded-full border-black'
+        />
+        <div className='pl-8 pt-8 font-sans font-black '>
+          <div className='text-left text-2xl font-bold'>{user.name}</div>
+          <div className='whitespace-pre-wrap break-words pt-8 text-left text-sm font-light'>
+            {user.description}
+          </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-2 gap-8 px-8 pt-8'>
+        {user &&
+          user.posts.map((post, index) => {
+            index % 3 === 0 ? (cardBackgroundColor = 'bg-orange') : ''
+            index % 3 === 1 ? (cardBackgroundColor = 'bg-navy') : 'bg-orange'
+            index % 3 === 2 ? (cardBackgroundColor = 'bg-blue') : 'bg-orange'
+            return (
+              <Link
+                href={post.url}
+                key={index}>
+                <Card
+                  size='small'
+                  icon=''
+                  bgColor={cardBackgroundColor}
+                  username=''
+                  img={post.image}
+                  description=''
+                  caption={post.caption}
+                />
+              </Link>
+            )
+          })}
+      </div>
+    </section>
+  )
+}
+
+export default MyProfileSection
