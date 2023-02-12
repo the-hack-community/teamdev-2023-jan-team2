ac3060a8dd3282f252285b638bb5581d7c96ac9b
zorro901 <zoro907@gmail.com>
2023-02-13 00:58:14 +0900

    :bug: fix(front): プロフィールページの表示を修正
    一部APIコードを追加

--- apps/web/src/app/my-profile/page.tsx
+++ apps/web/src/app/my-profile/page.tsx
@@ -1,53 +1,56 @@
-import MyProfileSection from 'ui/components/organisms/MyProfileSection'
-import Image from 'next/image'
+// import type { ImageInput, UserInput } from '@ca11-ope/config/schema'
 import { redirect } from 'next/navigation'
 import { getServerSession } from 'next-auth/next'
-import getSingleImage from '../../libs/image-util'
+import MyProfileSection from 'ui/components/organisms/MyProfileSection'
+// import getSingleImage from '../../libs/image-util'
 import { authOptions } from '../../pages/api/auth/[...nextauth]'
+import hero from 'assets/hero.webp'
 
-const MyProfilePage = async() => {
+const MyProfilePage = async () => {
   const session = await getServerSession(authOptions)
   if (!session) redirect('/')
-  const imageData = await getSingleImage(1)
+  console.log(session)
+  // TODO: SessionProviderからユーザデータを取得するように変更
+  // const imageData: ImageInput = await getSingleImage(1)
+  // const userData: UserInput = {
+  //   username: session.user?.name,
+  //   email: session.user?.email,
+  //   avatarUrl: session.user?.image,
+  // }
   const dummyData = {
     user: {
       name: 'にゃーんさん',
-      description: '自己紹介文です。',
-      icon: '/icon.png',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      icon: hero,
       posts: [
         {
-          image: '/sample.png',
+          image: hero,
           url: 'http://dummy.co.jp',
           caption: '画像キャプション1',
         },
         {
-          image: '/sample.png',
+          image: hero,
           url: 'http://dummy.co.jp',
-          caption: '画像キャプション2',
+          caption: '画像キャプション1',
         },
         {
-          image: '/sample.png',
+          image: hero,
           url: 'http://dummy.co.jp',
-          caption: '画像キャプション3',
+          caption: '画像キャプション1',
         },
         {
-          image: '/sample.png',
+          image: hero,
           url: 'http://dummy.co.jp',
-          caption: '画像キャプション4',
+          caption: '画像キャプション1',
         },
       ],
     },
   }
 
   return (
-    <div>
+    <div className='bg-orange h-screen pt-[120px]'>
       <MyProfileSection user={dummyData.user} />
-      <Image
-        src={`data:image/png;base64,${imageData}`}
-        alt=''
-        width={300}
-        height={300}
-      />
     </div>
   )
 }
