423a00dd424d26a7a79abdc0c3f4abf8932c99ca
zorro901 <zoro907@gmail.com>
2023-02-13 01:48:34 +0900

    :lipstick: style(front): ポストページの表示を修正

--- apps/web/src/app/upload/page.tsx
+++ apps/web/src/app/upload/page.tsx
@@ -1,17 +1,22 @@
-import 'styles/globals.css'
 import Image from 'next/image'
 import UploadSection from 'ui/components/organisms/UploadSection'
 import hero from 'assets/hero.webp'
 
 const App = () => {
   return (
-    <div className='bg-blue h-screen'>
-      <Image
-        src={hero}
-        alt='hero'
-        className='absolute top-0 left-0'
-      />
-      <UploadSection />
+    <div className='bg-blue relative -z-10 h-screen pt-48'>
+      <div className='w-full'>
+        <Image
+          src={hero}
+          alt='hero'
+          className='absolute -z-10 -mt-24'
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+      <div className='relative'>
+        <UploadSection />
+      </div>
     </div>
   )
 }
--- packages/ui/components/organisms/UploadSection/index.tsx
+++ packages/ui/components/organisms/UploadSection/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import MicIcon from 'atoms/MicIcon'
 
 const UploadSection = () => {
@@ -7,9 +9,9 @@ const UploadSection = () => {
   }
 
   return (
-    <section className='mx-auto mt-12 flex h-[678px] w-[951px] flex-col items-center justify-center rounded-3xl bg-yellow-500'>
+    <section className='bg-yellow mx-auto flex h-[678px] w-[951px] flex-col items-center justify-center rounded-3xl'>
       <MicIcon onClick={handleClick} />
-      <div className='text-center text-4xl text-orange-500'>イラストセイセイ</div>
+      <div className='text-orange text-center text-4xl'>イラストセイセイ</div>
     </section>
   )
 }
