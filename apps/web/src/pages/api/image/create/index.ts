469976e566c504da04fc5b0fe53e39081ab39c70
zorro901 <zoro907@gmail.com>
2023-02-16 12:11:43 +0900

    :sparkles: feat(front): 音声認識処理を追加

--- apps/web/src/pages/api/image/create/index.ts
+++ apps/web/src/pages/api/image/create/index.ts
@@ -1,21 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next/types'
 import { getServerSession } from 'next-auth/next'
+import { defaultNegativePrompt, defaultPrompt } from '../../../../const/prompt'
 import { authOptions } from '../../auth/[...nextauth]'
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const createImage = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerSession(req, res, authOptions)
   if (!session) res.end()
+  const { prompt, userId } = req.body
   const response = await fetch(process.env.GRAPHQL_ENDPOINT ?? '', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      query:
-        // TODO パラメータをテンプレートリテラルで変更出来るように修正
-        'mutation{imageCreate(input:{imageInput:{prompt:"test"steps:1clipSkip:2userId:1}}){image{prompt}}}',
+      query: `
+mutation {
+  imageCreate(
+    imageInput: {prompt: "{{{{1 ${prompt},character focus}}}} ${defaultPrompt}", clipSkip: 2, steps: 20, userId: ${userId}, description: "${prompt}", negativePrompt: "${defaultNegativePrompt}"}
+  ) {
+    image {
+      id
+    }
+  }
+}
+      `,
     }),
   })
   const createImageResponse = await response.json()
   res.json({ session: createImageResponse })
 }
+export default createImage
