4c5f27a8528edd14dbefac1976f0509e48fb344b
zorro901 <zoro907@gmail.com>
2023-02-18 15:00:26 +0900

    :bug: fix(front): 画像取得メソッドを修正

--- packages/ui/libs/image/get-single-image-by-id.ts
+++ packages/ui/libs/image/get-single-image-by-id.ts
@@ -1,14 +1,18 @@
 import type { ImageInput } from '@ca11-ope/config/schema'
 
 const getSingleImageById = async (id: number) => {
-  const response = await fetch(`${process.env.BASE_URL}/api/image/${id}`, {
+  const response = await fetch(`/api/image/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
     method: 'POST',
     body: JSON.stringify({
-      id,
+      options: 'id',
     }),
   })
   if (!response.ok) return null
   const result = await response.json()
+  if (!result) return null
   const ImageInput: ImageInput = result.imageById
   return ImageInput
 }
