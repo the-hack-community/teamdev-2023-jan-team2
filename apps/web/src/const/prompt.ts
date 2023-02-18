1d8e3bbb0c7d2b0851d40d1a8a24bad848189661
zorro901 <zoro907@gmail.com>
2023-02-18 15:03:24 +0900

    :bug: fix(front): デフォルトプロンプトを変更

--- apps/web/src/const/prompt.ts
+++ apps/web/src/const/prompt.ts
@@ -1,10 +1,8 @@
-export const defaultPrompt = `{anime screencap},{{{3d,weak 
-light}}},hidden hands,spread arms,building,((masterpiece)), (((best 
-quality))), ((ultra-detailed)), ((illustration)), ((disheveled hair)), 
-floating, beautiful detailed sky,blue sky, cinematic 
-lighting,((chromatic aberration)), ((caustic)), beautiful detailed 
-glow,dynamic angle,depth of field,light_leaks,Amazing,finely 
-detail,extremely detailed CG,Original,(sunlight),((an extremely 
-delicate and beautiful)),glowing,bright,{{{bluearchive}}},{{{strong contrast,3d,strong light}}}`
+export const defaultPrompt = `80sanimestyle, (((masterpiece))),(((best quality))),((ultra-detailed)),(illustration),(dynamic angle),
+((floating)),(paint),((disheveled hair)),
+(solo),(1girl), loli, ((small_breasts)), (((detailed anima face))),((beautiful detailed face)),collar,bare shoulders,
+white hair, ((colorful hair)),((streaked hair)),
+beautiful detailed eyes,(Gradient color eyes),(((colorful eyes))),
+(((colorful background))),(((high saturation))),(((surrounded by colorful splashes))),(((surrounded by colorful dot))),colorful bubble,((shining))`
 export const defaultNegativePrompt =
-  'nsfw, owres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry,missing fingers,bad hands,missing arms, long neck, Humpbacked ,greyscale'
+  'nsfw,nipples,(black skin),dark background,lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts,signature, watermark, username, blurry, artist name,bad feet'
